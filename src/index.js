1f0be37453c0ef9417c925a32131a0bec21e361a
Edward Angelo Salas <zyro670@gmail.com>
2022-04-18 21:40:43 -0400

    commit tree d8a4ade980066aa6760126a7893769824e2e8988

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.getElementById('dog-image-container');
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then(res => {
+            res.message.forEach((imgSrc) => {
+                const img = document.createElement('img');
+                img.src = imgSrc;
+                div.appendChild(img);
+            })
+        });
+});
+var ul = document.createElement('ul');
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+document.addEventListener('DOMContentLoaded', () => {
+    ul = document.getElementById('dog-breeds');
+    fetch(breedUrl)
+        .then(res => {
+            res.message.forEach((breedSrc) => {
+                const img = document.createElement('breed');
+                img.src = breedSrc;
+                ul.appendChild(img);
+            })
+        })
+});
